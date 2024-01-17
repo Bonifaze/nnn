539fe867f2f9f61020384fd0f6dae7cb5fe1a3ff
Ntami Egbe <ntamiegbe00@gmail.com>
2024-01-17 12:05:44 +0100

    feat: added responsive news slider

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Siwper JS
+
+let swiperCard = new Swiper(".card-content", {
+  loop: true,
+  spaceBetween: 12,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 3,
+    },
+    950: {
+      slidesPerView: 5,
+    },
+  },
+});
